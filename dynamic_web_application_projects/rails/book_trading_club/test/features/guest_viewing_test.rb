3a0050c81a6fa91d42f16419a76bca2d5b6c70c4
Richard <richard.wigley@gmail.com>
2016-10-06 13:07:34 +0100

    HighVoltage for static pages - home added

---
+++ dynamic_web_application_projects/rails/book_trading_club/test/features/guest_viewing_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+# GuestViewing
+#  - Guest user features
+#
+class GuestViewingTest < Capybara::Rails::TestCase
+  test "document title has been set" do
+    visit root_path
+    assert_selector "h1", text: "home page"
+  end
+end
