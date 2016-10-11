d6130ee45a6791dd7e247487f99e035b1d1d12ba
Richard <richard.wigley@gmail.com>
2016-10-11 16:12:33 +0100

    Feature Test stub for testing a guest can sign up

--- test/features/guest_viewing_test.rb
+++ test/features/guest_viewing_test.rb
@@ -8,4 +8,9 @@ class GuestViewingTest < Capybara::Rails::TestCase
     visit root_path
     assert_selector "h1", text: "ROE"
   end
+
+  test "a guest can sign up" do
+    skip('todo')
+    visit root_path
+  end
 end
