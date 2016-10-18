4307314fea359b32ec854c8ccc16ff0d4d4de5f3
Richard <richard.wigley@gmail.com>
2016-10-18 17:06:00 +0100

    Rename after functionality guest_viewing_test => registration_test

--- test/features/registration_test.rb
+++ test/features/registration_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
-# GuestViewing
-#  - Guest user features
+# Registration
+#  - Registration feature
 #
-class GuestViewingTest < Capybara::Rails::TestCase
+class RegistrationTest < Capybara::Rails::TestCase
   test "can view landing page" do
     visit root_path
     assert_selector "h1", text: "ROE"
