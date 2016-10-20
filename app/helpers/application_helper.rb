a0feff0c2695c579e805b2c22ee3a9edd075252c
Richard <richard.wigley@gmail.com>
2016-10-20 21:28:33 +0100

    Flash messages are styled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  MESSAGES = { notice: ['message message--notice', 'info-circle 2x'],
+               alert:  ['message message--alert', 'bell 2x'] }.freeze
+  def messages(name, msg)
+    message = MESSAGES[name.to_sym]
+    content_tag :div, class: message[0] do
+      content_tag :div, class: 'container' do
+        content_tag(:span, fa_icon(message[1], text: msg))
+      end
+    end
+  end
 end
