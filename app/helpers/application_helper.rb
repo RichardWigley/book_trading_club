5ab996e60fbdb0dcabc27bc3ed6188ed9e2e5cb6
Richard <richard.wigley@gmail.com>
2016-10-24 15:23:04 +0100

    css container => layout__w-wrapper to fit into layout.scss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   def messages(name, msg)
     message = MESSAGES[name.to_sym]
     content_tag :div, class: 'js-slow-time ' + message[0] do
-      content_tag :div, class: 'container' do
+      content_tag :div, class: 'layout__w-wrapper' do
         content_tag(:span, fa_icon(message[1], text: msg))
       end
     end
