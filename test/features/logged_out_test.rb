fc9c8683ef8f92db13b116e0469fb9756e66fc77
Richard <richard.wigley@gmail.com>
2016-10-24 21:23:22 +0100

    Logged out page was missing any feature tests.
    Even people leaving us deserve tests (just about)

---
+++ test/features/logged_out_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+# Logged Out
+#  - logged out user features
+#
+class LoggedOutTest < Capybara::Rails::TestCase
+  test "user can navigate to log in" do
+    visit logged_out_path
+    click_on 'Log in'
+
+    assert_equal('Log In - BookTradingClub', page.title)
+  end
+
+  test "user can navigate to sign up" do
+    visit logged_out_path
+    click_on 'Sign up'
+
+    assert_equal('Sign Up - BookTradingClub', page.title)
+  end
+end
