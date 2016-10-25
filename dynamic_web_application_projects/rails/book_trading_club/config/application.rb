d73e21b2103970e7f97788ef14b870574443f948
Richard <richard.wigley@gmail.com>
2016-10-25 14:42:53 +0100

    Rails test generator is not using spec or fixtures

--- dynamic_web_application_projects/rails/book_trading_club/config/application.rb
+++ dynamic_web_application_projects/rails/book_trading_club/config/application.rb
@@ -7,9 +7,16 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module BookTradingClub
+  # Application
+  #  - Rails::Application is responsible for executing all railties
+  #    and engine initializers
+  #
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |generator|
+      generator.test_framework :minitest, spec: false, fixture: false
+    end
   end
 end
