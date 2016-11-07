47fc1a52984a9d2d6960eadff41707028bf6034f
Richard <richard.wigley@gmail.com>
2016-11-07 11:20:30 +0000

    deploy staging use server_address and update nginx_server_name
    I have already changed from an ip_address to a FQDN so have
    renamed to server_address;
    
    The move away from an ip address allowed me to only change the
    ip address once for each new server - a change in the /etc/hosts.
    
    nginx_server_name was not a FQDN which stopped the staging server
    being seen after a deploy. This change makes NGINX direct
    yell.richard.wigley.co.uk to the puma application server instance.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,18 +5,18 @@
 #-------------------------------------------------------------------------------
 # Capistrano Standard environment settings
 #
-def ip_address
+def server_address
   'yell.richardwigley.co.uk'
 end
 
 set :stage, :production
 set :branch, 'master'
 
-role :app, ip_address
-role :web, ip_address
-role :db,  ip_address
+role :app, server_address
+role :web, server_address
+role :db,  server_address
 
-server ip_address, user: 'deployer', roles: %w(web app db), primary: true
+server server_address, user: 'deployer', roles: %w(web app db), primary: true
 set :rails_env, :production
 
 #-------------------------------------------------------------------------------
@@ -24,4 +24,4 @@ set :rails_env, :production
 #
 # Unicorn
 #
-set :nginx_server_name, 'richardwigley.co.uk'
+set :nginx_server_name, server_address
