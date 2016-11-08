6af855ce75f931a04e4f9ed838fcac7d4360e6ce
Richard <richard.wigley@gmail.com>
2016-11-08 12:15:52 +0000

    create_account_and_login returns created account

--- test/support/create_account_and_login.rb
+++ test/support/create_account_and_login.rb
@@ -2,9 +2,10 @@
 #  - feature tests helper method for creating and signing a user in
 #
 def create_account_and_login(email: 'user@example.com', password: 'secret')
-  Account.create(email: email, password: password)
+  account = Account.create(email: email, password: password)
   visit new_account_session_path
   fill_in 'Email', with: 'user@example.com'
   fill_in 'Password', with: 'secret'
   click_on 'Log in'
+  account
 end
